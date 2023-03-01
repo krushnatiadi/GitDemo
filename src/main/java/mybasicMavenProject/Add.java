c717d09691a18b8de39955a56b66d38c2500e4ed
Krushna <krushna.cfg@gmail.com>
2023-03-01 12:01:08 +0530

    Architect Specila developement

---
+++ src/main/java/mybasicMavenProject/Add.java
@@ -0,0 +1,21 @@
+package mybasicMavenProject;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello");
+System.out.println("hh");
+System.out.println("ff");
+System.out.println("gg");
+
+System.out.println("ee");
+System.out.println("rr");
+System.out.println("tt");
+System.out.println("ss");
+System.out.println("cc");
+System.out.println("cv");
+
+	}
+
+}
